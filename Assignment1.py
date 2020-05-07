1ba288829ab7d34e69bdbf37d3f37dae8e1cca0d
unknown <ripal594@gmail.com>
2020-05-07 16:15:43 -0400

    This is the first assignment of Captson Course

---
+++ Assignment1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #This is for Data Science Capston Project!!
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[9]:
+
+
+print("Hello Capston Project Course!")
+
+
+# In[ ]:
+
+
+
+
